a2b6075a3f8e68d1631c9dace293e60e35ed5ce5
Raphael Paiva <raphaeldpaiva@gmail.com>
2012-03-20 19:13:55 -0300

    initial commit

---
+++ src/test/java/org/eyequery/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package org.eyequery;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HelloWorldTest
+{
+   @Test
+   public void testGetText() {
+      HelloWorld fixture = new HelloWorld();
+      assertEquals("Hello, World!", fixture.getText());
+   }
+}
